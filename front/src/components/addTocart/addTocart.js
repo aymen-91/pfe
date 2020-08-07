d28d755c05b134dfb68b41c9deb0ee829a0c16f9
aymen-91 <47060385+aymen-91@users.noreply.github.com>
2020-08-08 00:49:12 +0100

    Add files via upload

---
+++ front/src/components/addTocart/addTocart.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import React, { Component } from 'react'
+ 
+import { connect } from 'react-redux'
+
+export default class addTocart extends Component {
+    state = { cmd:[] };
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default connect(null,{addTocart})(addTocart)
